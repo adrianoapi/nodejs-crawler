e4e9c0648f472a75ce62e2cbfdedc302e8780293
Adriano API <adrianoapi@hotmail.com>
2018-07-30 15:59:40 -0300

    add estructure

---
+++ index.js
@@ -0,0 +1,18 @@
+
+var request = require('request');
+var cheerio = require('cheerio');
+
+request('https://www.imdb.com/chart/moviemeter', function(err, response, body){
+	if(err) console.log('Erro:' + err);
+
+	var $ = cheerio.load(body);
+
+	$('.lister-list tr').each(function(){
+		var title  = $(this).find('.titleColumn a').text().trim();
+		var rating = $(this).find('.ratingColumn strong').text().trim();
+		
+		console.log('Título: ' + title + ' -  Nota: ' + rating);
+
+	});
+
+})
